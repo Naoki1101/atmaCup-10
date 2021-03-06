a1f36ea6b16d63e9cc1ef99da279a575e2decab2
Naoki1101 <naoki.narimatsu111@gmail.com>
2021-03-06 21:40:25 +0900

    lightgbm_008_20210306213905_1.0290

--- bin/run.sh
+++ bin/run.sh
@@ -8,4 +8,5 @@ cd ../src
 # python train.py -m 'lightgbm_004' -c 'optuna params'
 # python train.py -m 'lightgbm_005' -c 'drop outlier'
 # python train.py -m 'lightgbm_006' -c 'custom_002'
-python train.py -m 'lightgbm_007' -c 'custom_003'
+# python train.py -m 'lightgbm_007' -c 'custom_003'
+python train.py -m 'lightgbm_008' -c 'custom_004'
