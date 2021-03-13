3948b15ba9b4f315b1edb6cdc76ceb74e626abf8
Naoki1101 <naoki.narimatsu111@gmail.com>
2021-03-13 15:14:14 +0900

    [EXP] lightgbm_019_20210313151218_1.0052

--- bin/preprocess.sh
+++ bin/preprocess.sh
@@ -3,6 +3,6 @@
 cd ../src
 
 # python preprocess/01_convert_feather.py
-# python preprocess/02_get_target_and_id.py
+python preprocess/02_get_target_and_id.py
 # python preprocess/03_extract_drop_idx.py
-python preprocess/04_calc_residual.py
+# python preprocess/04_calc_residual.py
--- src/preprocess/02_get_target_and_id.py
+++ src/preprocess/02_get_target_and_id.py
@@ -1,5 +1,6 @@
 import sys
 
+import numpy as np
 import pandas as pd
 
 sys.path.append("../src")
@@ -11,6 +12,9 @@ def main():
 
     for col in const.ID_COLS + const.TARGET_COLS:
         train_df[[col]].to_feather(const.FEATURE_DIR / f"{col}.feather")
+
+    train_df["likes_bin"] = pd.cut(np.log1p(train_df["likes"]), bins=10, labels=False)
+    train_df[["likes_bin"]].to_feather(const.FEATURE_DIR / f"likes_bin.feather")
 
 
 if __name__ == "__main__":
